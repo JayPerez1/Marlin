92d48a5faa01f8b8b4b0c2b4d8287d0241ad8896
JayPerez1 <jay.perez1@gmail.com>
2021-03-13 12:30:35 -0500

    A New Dawn

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(02-09-2021 SKR 1.4 Turbo)"    //BluJay // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(03-13-2021 SKR 1.4 Turbo)"    //BluJay // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
